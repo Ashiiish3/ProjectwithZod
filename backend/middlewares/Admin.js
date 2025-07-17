6917960d9c8d11eb77d35ba22befa81a4fb1148c
Ashish Sharma <143114291+Ashiiish3@users.noreply.github.com>
2025-07-17 18:11:10 +0530

    Delete backend directory

--- backend/middlewares/Admin.js
+++
@@ -1,8 +0,0 @@
-const Admin = (req, res, next) => {
-    if(req.user.role !== "admin"){
-        return res.status(400).json({message: "You don't have permission to see all Notes."})
-    }
-    next();
-}
-
-module.exports = Admin;
